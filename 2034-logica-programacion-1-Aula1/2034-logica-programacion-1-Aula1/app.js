3bf2121f183fbe3ddf870c5dd3e1efc2ab917d22
DrRobert12 <robertcardz.97@gmail.com>
2025-02-01 09:50:22 -0300

    // Content:

--- 2034-logica-programacion-1-Aula1/2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/2034-logica-programacion-1-Aula1/app.js
@@ -1,20 +1,23 @@
 let numeroSecreto = 7;
 let numeroUsuario = 0;
 let intentos = 1;
+let palabrasVeces = "vez";
+let 
 while (numeroUsuario != numeroSecreto) {
     let numeroUsuario = prompt("Me indicas un numero por favor, entre el 1 y el 10:");
 
     console.log(numeroUsuario);
-    intentos = intentos + 1;
-    //comentario//
-
+    
     if (numeroUsuario == numeroSecreto) {
-        alert(`Acertaste, el numero era: ${numeroUsuario}. Lo lograste en ${intentos} intentos`);
+        alert(`Acertaste, el numero era: ${numeroUsuario}. Lo lograste en ${intentos} ${palabrasVeces}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El numero secreto es menor');
         } else {
             alert('El numero secreto es mayor');
-        }
+        
+        intentos = intentos + 1;    
+        palabrasVeces = "veces";
+    };
     }//se incrementa el contador a medida que va intentando//
 }

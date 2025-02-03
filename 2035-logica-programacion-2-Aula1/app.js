5ef287b9e9592654e5b3af43417ee23cebe1812e
DrRobert12 <robertcardz.97@gmail.com>
2025-02-03 09:57:31 -0300

    Fase2

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,2 +1,5 @@
-document.querySelector('h1');
+let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Juego del número secreto';
+
+let parrado = document.querySelector('p');
+parrafo.innerHTML = 'Adivina el número secreto que está entre 1 y 10';

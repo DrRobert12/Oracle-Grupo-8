6571e96cdcff3c684c9411e7beb48054601535ff
DrRobert12 <robertcardz.97@gmail.com>
2025-02-05 10:25:26 -0300

    05/02

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,10 +1,21 @@
+let numeroSecreto = generarNumeroSecreto();
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
-function intentoDeUsuario() {
-    alert('Click desde el boton');{}    
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log (typeof (numeroDeUsuario));
+    console.log (numeroSecreto);
+    console.log (typeof (numeroSecreto));
+    console.log (numeroDeUsuario);
+    console.log (numeroDeUsuario === numeroSecreto); 
+    return;
 }
-
+function generarNumeroSecreto() {
+    return numeroSecreto = Math.floor(Math.random() * 10) + 1;
+     
+}
 asignarTextoElemento('h1', 'Juego del número secreto');
 asignarTextoElemento('p', 'Adivina el número secreto que está entre 1 y 10');
